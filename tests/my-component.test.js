6b12374524ef6a60bc5071dc05c7ef62819f8fb1
Rafael Minguet <rminguet@gmail.com>
2017-12-18 23:13:48 +0100

    Day 12/12/2017

--- tests/my-component.test.js
+++ tests/my-component.test.js
@@ -1,19 +1,42 @@
 import test from 'tape';
 
 // Component to test
-const Character = function ( options = {}) {
+const Character = function (options = {}) {
 
-    let life = options.life || 100;
+    const {life = 100, weapon} = options;
+    const stats = {life, weapon};
 
     const getLife = function () {
-        return life;
+        return stats.life;
     };
 
-    return {getLife}
+    const getWeapon = function () {
+        return stats.weapon;
+    };
+
+    return {getLife, getWeapon};
 };
 
-const Weapon = function ( options = {}) {
+const Ammunition = function (options = {}) {
+    const {damage = 4} = options;
+    const stats = {damage};
 
+    const getDamage = function () {
+        return stats.damage;
+    };
+
+    return {damage: getDamage()};
+};
+
+const Weapon = function (options = {}) {
+    const {ammo, capacity, ammoPerShot} = options;
+    const stats = {ammo, capacity, ammoPerShot};
+
+    const getStats = function () {
+        return stats;
+    };
+
+    return {getStats};
 };
 
 // Tests
@@ -56,15 +79,44 @@ test(`-------- Testing demon's constructor...`, (assert) => {
     assert.end();
 });
 
+test(`-------- Testing ammunition's constructor...`, (assert) => {
+    const message = `Retrieving generic ammunition's damage`;
+    const expected = 4;
+
+    const ammunition = Ammunition({damage: 4});
+
+    const actual = ammunition.damage;
+
+    assert.deepEqual(actual, expected, message);
+
+    assert.end();
+});
+
 test(`-------- Testing weapon's constructor...`, (assert) => {
     const message = `Retrieving generic weapon stats`;
-    const expected = {ammo: null, capacity: null, ammoPerShot: null, damage: 4};
+    const expected = {ammo: {damage: 4}, capacity: null, ammoPerShot: null};
 
-    const weapon = Weapon({ammo: null, capacity: null, ammoPerShot: null, damage: 4});
+    const ammo = Ammunition({damage: 4});
+    const weapon = Weapon({ammo, capacity: null, ammoPerShot: null});
 
     const actual = weapon.getStats();
 
-    assert.equal(actual, expected, message);
+    assert.deepEqual(actual, expected, message);
+
+    assert.end();
+});
+
+test(`-------- Testing demon with weapon...`, (assert) => {
+    const message = `Retrieving generic weapon from a generic demon`;
+    const expected = {ammo: {damage: 50}, capacity: null, ammoPerShot: null};
+
+    const ammo = Ammunition({damage: 50});
+    const weapon = Weapon({ammo, capacity: null, ammoPerShot: null});
+    const demon = Character({life: 100, weapon});
+
+    const actual = demon.getWeapon().getStats();
+
+    assert.deepEqual(actual, expected, message);
 
     assert.end();
 });
