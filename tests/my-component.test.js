2542596bd9bf61e0d6093a6bef7eb7e8f5f42452
Rafael Minguet <rminguet@gmail.com>
2018-01-21 21:33:30 +0100

    Weapon's collection

--- tests/my-component.test.js
+++ tests/my-component.test.js
@@ -41,6 +41,17 @@ const Ammunition = function (options = {}) {
     return {damage: getDamage(), type: getType()};
 };
 
+const WEAPONS = {
+    FISTS: {name: 'fists', ammo: Ammunition({damage: 15}), ammoPerShot: null},
+    CHAINSAW: {name: 'chainsaw', ammo: Ammunition({damage: 30}), ammoPerShot: null},
+    PISTOL: {name: 'pistol', ammo: Ammunition({type: 'bullet', damage: 15}), ammoPerShot: 1},
+    SHOTGUN: {name: 'shotgun', ammo: Ammunition({type: 'shell', damage: 50}), ammoPerShot: 1},
+    CHAINGUN: {name: 'chaingun', ammo: Ammunition({type: 'bullet', damage: 150}), ammoPerShot: 10},
+    ROCKET_LAUNCHER: {name: 'rocketLauncher', ammo: Ammunition({type: 'rocket', damage: 200}), ammoPerShot: 1},
+    PLASMAGUN: {name: 'plasmagun', ammo: Ammunition({type: 'plasma', damage: 200}), ammoPerShot: 10},
+    BFG9000: {name: 'bfg9000', ammo: Ammunition({type: 'plasma', damage: 1000}), ammoPerShot: 50}
+};
+
 const Weapon = function (options = {}) {
     const {ammo, ammoPerShot} = options;
     const stats = {ammo, ammoPerShot};
@@ -151,23 +162,210 @@ test(`-------- Testing player's creation...`, (assert) => {
     assert.end();
 });
 
-/*
-const WEAPONS = {
-    fists: {name: 'fists', ammo: Ammunition({damage: 4}), capacity: null, ammoPerShot: null},
-    chainsow: {}
-};
-*/
-/*
 test(`-------- Testing weapon collection (fists)...`, (assert) => {
-    const message = `Retrieving fists' characteristics from weapon's collection`;
-    const expected =;
+    const messageName = `Retrieving fists' name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving fists' ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving fists' damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving fists' ammo/shot from weapon's collection`;
 
-    const player = Player({armor: 50, weapons: [WEAPONS.fists, WEAPONS.chainsow]});
+    const expectedName = 'fists';
+    const expectedAmmunitionName = 'none';
+    const expectedDamage = 15;
+    const expectedAmmoPerShot = null;
 
-    const actual = player.getLife();
+    const fists = WEAPONS.FISTS;
 
-    assert.equal(actual, expected, message);
+    const actualName = fists.name;
+    const actualAmmunitionName = fists.ammo.type;
+    const actualDamage = fists.ammo.damage;
+    const actualAmmoPerShot = fists.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
 
     assert.end();
 });
-*/
+
+test(`-------- Testing weapon collection (chainsaw)...`, (assert) => {
+    const messageName = `Retrieving chainsaw's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving chainsaw's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving chainsaw's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving chainsaw's ammo/shot from weapon's collection`;
+
+    const expectedName = 'chainsaw';
+    const expectedAmmunitionName = 'none';
+    const expectedDamage = 30;
+    const expectedAmmoPerShot = null;
+
+    const chainsaw = WEAPONS.CHAINSAW;
+
+    const actualName = chainsaw.name;
+    const actualAmmunitionName = chainsaw.ammo.type;
+    const actualDamage = chainsaw.ammo.damage;
+    const actualAmmoPerShot = chainsaw.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
+
+test(`-------- Testing weapon collection (pistol)...`, (assert) => {
+    const messageName = `Retrieving pistol's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving pistol's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving pistol's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving pistol's ammo/shot from weapon's collection`;
+
+    const expectedName = 'pistol';
+    const expectedAmmunitionName = 'bullet';
+    const expectedDamage = 15;
+    const expectedAmmoPerShot = 1;
+
+    const pistol = WEAPONS.PISTOL;
+
+    const actualName = pistol.name;
+    const actualAmmunitionName = pistol.ammo.type;
+    const actualDamage = pistol.ammo.damage;
+    const actualAmmoPerShot = pistol.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
+
+test(`-------- Testing weapon collection (shotgun)...`, (assert) => {
+    const messageName = `Retrieving shotgun's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving shotgun's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving shotgun's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving shotgun's ammo/shot from weapon's collection`;
+
+    const expectedName = 'shotgun';
+    const expectedAmmunitionName = 'shell';
+    const expectedDamage = 50;
+    const expectedAmmoPerShot = 1;
+
+    const shotgun = WEAPONS.SHOTGUN;
+
+    const actualName = shotgun.name;
+    const actualAmmunitionName = shotgun.ammo.type;
+    const actualDamage = shotgun.ammo.damage;
+    const actualAmmoPerShot = shotgun.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
+
+test(`-------- Testing weapon collection (chaingun)...`, (assert) => {
+    const messageName = `Retrieving chaingun's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving chaingun's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving chaingun's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving chaingun's ammo/shot from weapon's collection`;
+
+    const expectedName = 'chaingun';
+    const expectedAmmunitionName = 'bullet';
+    const expectedDamage = 150;
+    const expectedAmmoPerShot = 10;
+
+    const chaingun = WEAPONS.CHAINGUN;
+
+    const actualName = chaingun.name;
+    const actualAmmunitionName = chaingun.ammo.type;
+    const actualDamage = chaingun.ammo.damage;
+    const actualAmmoPerShot = chaingun.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
+
+test(`-------- Testing weapon collection (rocket launcher)...`, (assert) => {
+    const messageName = `Retrieving rocket launcher's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving rocket launcher's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving rocket launcher's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving rocket launcher's ammo/shot from weapon's collection`;
+
+    const expectedName = 'rocketLauncher';
+    const expectedAmmunitionName = 'rocket';
+    const expectedDamage = 200;
+    const expectedAmmoPerShot = 1;
+
+    const rocketLauncher = WEAPONS.ROCKET_LAUNCHER;
+
+    const actualName = rocketLauncher.name;
+    const actualAmmunitionName = rocketLauncher.ammo.type;
+    const actualDamage = rocketLauncher.ammo.damage;
+    const actualAmmoPerShot = rocketLauncher.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
+
+test(`-------- Testing weapon collection (plasmagun)...`, (assert) => {
+    const messageName = `Retrieving plasmagun's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving plasmagun's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving plasmagun's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving plasmagun's ammo/shot from weapon's collection`;
+
+    const expectedName = 'plasmagun';
+    const expectedAmmunitionName = 'plasma';
+    const expectedDamage = 200;
+    const expectedAmmoPerShot = 10;
+
+    const plasmagun = WEAPONS.PLASMAGUN;
+
+    const actualName = plasmagun.name;
+    const actualAmmunitionName = plasmagun.ammo.type;
+    const actualDamage = plasmagun.ammo.damage;
+    const actualAmmoPerShot = plasmagun.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
+
+test(`-------- Testing weapon collection (BFG 9000)...`, (assert) => {
+    const messageName = `Retrieving BFG 9000's name from weapon's collection`;
+    const messageAmmunitionName = `Retrieving BFG 9000's ammunition's name from weapon's collection`;
+    const messageDamage = `Retrieving BFG 9000's damage from weapon's collection`;
+    const messageAmmoPerShot = `Retrieving BFG 9000's ammo/shot from weapon's collection`;
+
+    const expectedName = 'bfg9000';
+    const expectedAmmunitionName = 'plasma';
+    const expectedDamage = 1000;
+    const expectedAmmoPerShot = 50;
+
+    const bfg9000 = WEAPONS.BFG9000;
+
+    const actualName = bfg9000.name;
+    const actualAmmunitionName = bfg9000.ammo.type;
+    const actualDamage = bfg9000.ammo.damage;
+    const actualAmmoPerShot = bfg9000.ammoPerShot;
+
+    assert.equal(actualName, expectedName, messageName);
+    assert.equal(actualAmmunitionName, expectedAmmunitionName, messageAmmunitionName);
+    assert.equal(actualDamage, expectedDamage, messageDamage);
+    assert.equal(actualAmmoPerShot, expectedAmmoPerShot, messageAmmoPerShot);
+
+    assert.end();
+});
